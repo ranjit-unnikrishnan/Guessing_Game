a75bbc65778bb63bd06e6b6258c03fff9f4d7146
Ranjit Unnikrishnan <ranjitu@amazon.com>
2019-02-21 23:25:42 -0800

    Commiting First Time

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var user_answer=ask_user("Are you ready to play a game? Enter Yes or No");
+console_log(user_answer);
+
+if(user_answer==="YES" || user_answer === "Yes" || user_answer === "yes")
+{
+    alert("Lets Have some Fun!");
+}
+else
+{
+    alert("Ok Then!");
+}
+
+
