efe303d2144dfa9738822625558062c4ef6e1756
Unnikrishnan <ranjitu@186590df9677.ant.amazon.com>
2019-03-01 00:50:15 -0800

    Modified Javascript file

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if(user_answer==="YES" || user_answer === "Yes" || user_answer === "yes")
     alert("Lets Have some Fun!");
 
     var movie_name = prompt("Which was the most costly movie ever made?");
-    console.log(movie_name);
+    console.log("Movie Name Entered by User:",movie_name);
 
     if(movie_name==="Pirates" || movie_name === "pirates" || movie_name === "PIRATES")
     {
@@ -17,14 +17,14 @@ if(user_answer==="YES" || user_answer === "Yes" || user_answer === "yes")
         alert("You Guessed it!!Now for the next question");
 
         var budget = prompt("What was the estimated cost in millions?");
-        console.log(budget);
+        console.log("Budget of the Movie: " + budget + " Million");
 
-        if (budget=== 385)
+        if (budget === "385")
         {
             alert("You Guessed it!!Now for the next question");
 
             var director = prompt("Who was the director of the movie?");
-            console.log(director);
+            console.log("Name of the Director: ", director);
 
             if(director="Rob Marshall")
             {
